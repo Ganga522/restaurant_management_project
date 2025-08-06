a892f83b5329daee58640b522623de88f34bb1a7
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-06 22:38:01 +0530

    20913

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import status
 
 from .models import Item
@@ -24,3 +25,24 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_menu(request):
+    menu=[
+        {
+            "name":"Pizza",
+            "description":"Classic cheese and tomato pizza with fresh basil.",
+            "price":250
+        },
+        {
+            "name":"Paneer Butter Masala",
+            "description":"Cottage cheese in a creamy tomato gravy",
+            "price":180
+        },
+        {
+            "name":"Veg Biryani",
+            "description":"Aromatic basmati rice with mixed vegetables and spices.",
+            "price":200
+        },
+    ]
+    return Response(menu)
