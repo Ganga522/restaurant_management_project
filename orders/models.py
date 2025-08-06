394c03dd080f532cd263d639e73279ede34b66fc
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-06 23:10:20 +0530

    20911

--- orders/models.py
+++ orders/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from products.models import Menu
+
+class Order(models.Model):
+    STATUS_CHOICES=[
+        ('PENDING','Pending'),
+        ('CONFIRMED','Confirmed'),
+        ('DELIVERED','Delivered'),
+        ('CANCELLED','Cancelled'),
+    ]
+
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    order_item=models.ForeignKey(Menu,on_delete=models.CASCADE)
+    total_amount=models.DecimalField(max_length=8,decimal_places=2)
+    order_status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='PENDING')
+    order_date=models.DateTimeField(auto_now_add=True)
