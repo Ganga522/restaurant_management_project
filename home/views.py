dc38ad67d15c2f12ee72af7f2ce1ab0ba6ed6f05
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-06 22:49:18 +0530

    20910

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+    response=requests.get('http://localhost:8000/api/products/menu/')
+    menu=response.json() if response.status_code == 200 else []
+    return render(request,'home/index.html',{'menu':menu})
