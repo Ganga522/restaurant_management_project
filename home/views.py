17840fff080a04b5ec04e11698826280e15dabc9
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-08 19:44:33 +0530

    24355

--- home/views.py
+++ home/views.py
@@ -9,3 +9,8 @@ def homepage(request):
     menu=response.json() if response.status_code == 200 else []
     restaurant_name=getattr(settings,'RESTAURANT_NAME','MY RESTAURANT')
     return render(request,'home/index.html',{'menu':menu,'restaurant':restaurant_name})
+
+def about_us(request):
+    return render(request,'home/about.html')
+
+    
