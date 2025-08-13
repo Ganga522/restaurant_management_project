e53cb0701fa5e66d7ab766dd368144c3eb814ba6
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-13 17:04:12 +0530

    30483

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,18 @@
-from django.http.import JsonResponse
-from rest_framework import APIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
 class MenuItemListView(APIView):
+    """
+    View to return a list of hardcoded menu items.
+    Later,this will be replaced with database queries.
+
+    """
+
     def get(self,request):
+        #Hardcoded menu items for now
         menu_items=[
             {"id":1,"name":"Margherita Pizza","price":8.99},
             {"id":2,"name":"Veggie Burger","price":6.49},
             {"id":3,"name":"Pasta Alfredo","price":7.99},
         ]
-        return JsonResponse(menu_items,safe=False)
+        return Resonse(menu_items)
