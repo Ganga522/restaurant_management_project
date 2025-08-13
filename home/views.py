28cd2f51e64f252bca54b825ea54c379f0f5eebd
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-13 22:53:27 +0530

    30486

--- home/views.py
+++ home/views.py
@@ -7,13 +7,24 @@ from django.conf import settings
 def homepage(request):
     response=requests.get('http://localhost:8000/api/products/menu/')
     menu=response.json() if response.status_code == 200 else []
+
     restaurant_name=getattr(settings,'RESTAURANT_NAME','MY RESTAURANT')
-    return render(request,'home/index.html',{'menu':menu,'restaurant':restaurant_name})
+    phone_number=getattr(settings,'RESTAURANT_PHONE_NUMBER','+91 9381253610')
+
+    return render(request,'home/index.html',{
+        'menu':menu,
+        'restaurant':restaurant_name,
+        'phone_number':phone_number
+    })
 
 def about_us(request):
-    return render(request,'home/about.html')
+    return render(request,'home/about.html',{
+        'restuarant_name':getattr(settings,'RESTAURANT_NAME','MY RESTAURANT'),
+        'phone_number':getattr(settings,'RESTAURANT_PHONE_NUMBER','+91 9381253611')
+    })
 
 def contact_view(request):
     return render(request,'home/contact.html',{
-        'restuarant_name':'My Awesome Restuarant'
+        'restuarant_name':getattr(settings,'RESTUARANT','MY AWESOME Restaurant'),
+        'phone_number':getattr(settings,'RESTAURANT_PHONE_NUMBER','+91 9381253611')
     })
