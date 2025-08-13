7d0b066dae83a7b233cf0d477d8db0515b57cc82
Kataru Gangadhar <142211368+Ganga522@users.noreply.github.com>
2025-08-13 16:59:05 +0530

    30483

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http.import JsonResponse
+from rest_framework import APIView
 
 # Create your views here.
+class MenuItemListView(APIView):
+    def get(self,request):
+        menu_items=[
+            {"id":1,"name":"Margherita Pizza","price":8.99},
+            {"id":2,"name":"Veggie Burger","price":6.49},
+            {"id":3,"name":"Pasta Alfredo","price":7.99},
+        ]
+        return JsonResponse(menu_items,safe=False)
